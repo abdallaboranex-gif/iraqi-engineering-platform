51bf4f151abf0f34c38bc1b0c3a614601cae20c5
abdallaboranex-gif <abdalla.boranex@gmail.com>
2026-07-27 16:21:16 +0300

    Create flag.py

---
+++ core_layout/navbar/nav_iraqi_flag/flag.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def render_iraqi_flag():
+    """برنامج مستقل لعرض العلم العراقي كرمز سيادي ثابت."""
+    st.markdown("<div style='font-size: 26px; padding-top: 5px; text-align: center;'>🇮🇶</div>", unsafe_allow_html=True)
